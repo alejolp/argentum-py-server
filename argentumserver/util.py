8c31888077b4f35e4d3e0f8be97c3a287d3ec934
Alejandro Santos <alejolp@alejolp.com.ar>
2011-05-23 03:07:39 -0300

    Licencia

--- argentumserver/util.py
+++ argentumserver/util.py
@@ -1,5 +1,27 @@
 # -*- coding: utf-8 -*-
 
+"""
+    AONX Server - Pequeño servidor de Argentum Online.
+    Copyright (C) 2011 Alejandro Santos <alejolp@alejolp.com.ar>
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+"""
+
+"""
+Aca va todo lo que no encaja en otros modulos.
+"""
+
 from ConfigParser import SafeConfigParser
 
 def debug_print(*args):
@@ -32,3 +54,4 @@ class MyConfigParser(SafeConfigParser):
         if "'" in val:
             val = val.split("'", 1)[0]
         return int(val)
+
